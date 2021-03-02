67949a9afde331aed63da22eb2ec0fb4ed8ace1d
sahil jaryal <60871687+sahil199926@users.noreply.github.com>
2021-03-03 01:16:43 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,25 @@
+
+
+var privious=0;
+
+var string_eq="";
+
+var res=0;
+function ev(a){   
+    if(a=="c") {string_eq="";}
+    else if(a=="d"){string_eq=string_eq.slice(0,-1);}
+    else{
+     string_eq+=a;}
+     
+    
+    document.getElementById("row_1").innerHTML=string_eq;
+}
+
+function cal(){
+    res+=eval(string_eq);
+   document.getElementById("row_1").innerHTML=res;
+   document.getElementById("priviou").innerHTML=res;
+  string_eq=res;
+   
+   
+}
